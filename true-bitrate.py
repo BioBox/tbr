fbd46a168f41899abcbc3b7c58d560d11b127e4d
Daniel J. Perry <dperry45@gatech.edu>
2021-06-19 17:43:44 -0400

    Fix indentation

--- true-bitrate.py
+++ true-bitrate.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # uncomment for debugging:
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
 import numpy
 from scipy.fftpack import rfft
 from scipy.io.wavfile import read
@@ -10,42 +11,41 @@ from scipy.signal import hann
 import sys
 import warnings
 
-
 def moving_average(a, w):
-  # calculate moving average
-  window = numpy.ones(int(w)) / float(w)
-  r = numpy.convolve(a, window, 'valid')
-  # len(a) = len(r) + w
-  a = numpy.empty((int(w / 2)))
-  a.fill(numpy.nan)
-  b = numpy.empty((int(w - len(a))))
-  b.fill(numpy.nan)
-  # add nan arrays to equal input and output length
-  return numpy.concatenate((a, r, b))
+    # calculate moving average
+    window = numpy.ones(int(w)) / float(w)
+    r = numpy.convolve(a, window, 'valid')
+    # len(a) = len(r) + w
+    a = numpy.empty((int(w / 2)))
+    a.fill(numpy.nan)
+    b = numpy.empty((int(w - len(a))))
+    b.fill(numpy.nan)
+    # add nan arrays to equal input and output length
+    return numpy.concatenate((a, r, b))
 
 
 def find_cutoff(a, dx, diff, limit):
-  for i in range(1, int(a.shape[0] - dx)):
-    if a[-i] / a[-1] > limit:
-      break
+    for i in range(1, int(a.shape[0] - dx)):
+        if a[-i] / a[-1] > limit:
+            break
     if a[int(-i - dx)] - a[-i] > diff:
-      return a.shape[0] - i - dx
-  return a.shape[0]
+        return a.shape[0] - i - dx
+    return a.shape[0]
 
 
 # print usage if no argument given
 if len(sys.argv[1:]) < 1:
-  print('usage %s audio_file.wav' % (sys.argv[0]))
-  sys.exit(1)
+    print('usage %s audio_file.wav' % (sys.argv[0]))
+    sys.exit(1)
 
 # read audio samples and ignore warnings, print errors
 try:
-  with warnings.catch_warnings():
-    warnings.simplefilter('ignore')
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
     input_data = read(sys.argv[1])
 except IOError as e:
-  print(e[1])
-  sys.exit(e[0])
+    print(e[1])
+    sys.exit(e[0])
 
 # process data
 freq = input_data[0]
@@ -58,11 +58,11 @@ spectrum = [0] * freq
 
 # run over the seconds (max 30)
 for t in range(0, seconds - 1):
-  # apply hanning window
-  window = hann(freq)
-  audio_second = audio[t * freq:(t + 1) * freq, channel] * window
-  # do fft to add second to frequency spectrum
-  spectrum += abs(rfft(audio_second))
+    # apply hanning window
+    window = hann(freq)
+    audio_second = audio[t * freq:(t + 1) * freq, channel] * window
+    # do fft to add second to frequency spectrum
+    spectrum += abs(rfft(audio_second))
 
 # calculate average of the spectrum
 spectrum /= seconds
@@ -76,34 +76,29 @@ cutoff = find_cutoff(spectrum, freq / 50, 1.25, 1.1)
 print('%i kHz' % (cutoff / 2000))
 print('(%s kHz)' % (cutoff / 2000.0))
 if (cutoff // 2000) == 11:
-  print('64 kbps')
+    print('64 kbps')
 elif (cutoff // 2000) == 16:
-  print('128 kbps')
+    print('128 kbps')
 elif (cutoff // 2000) == 19:
-  print('192 kbps')
+    print('192 kbps')
 elif (cutoff // 2000) == 20:
-  print('320 kbps')
+    print('320 kbps')
 elif (cutoff // 2000) == 22:
-  print('500 kbps')
+    print('500 kbps')
 elif (cutoff * 100) / freq > 90:
-  print('lossless')
+    print('lossless')
 else:
-  print("""
-  11 kHz: 64 kbps
-  16 kHz: 128 kbps
-  19 kHz: 192 kbps
-  20 kHz: 320 kbps
-  22 kHz: 500 kbps
-  """)
+    print("No Match")
 
 # debugging only:
 if 'plt' in globals():
-  # plot
-  plt.plot(spectrum)
-  # label the axes
-  plt.ylabel('Magnitude')
-  plt.xlabel('Frequency')
-  # set the title
-  plt.title('Spectrum')
-  plt.axis((0, 45000, 0, 10))
-  plt.show()
+    print("Cutoff found to be {}".format(cutoff))
+    # plot
+    plt.plot(spectrum)
+    # label the axes
+    plt.ylabel('Magnitude')
+    plt.xlabel('Frequency')
+    # set the title
+    plt.title('Spectrum')
+    plt.axis((0, 45000, 0, 10))
+    plt.show()
