b21aabf0354a7cc423ebbc8bda41c6e0e08ca37e
Daniel J. Perry <dperry45@gatech.edu>
2021-09-03 10:06:52 -0400

    Change algorithm to use a spectrogram
    Signed-off-by: Daniel J. Perry <dperry45@gatech.edu>

--- true-bitrate.py
+++ true-bitrate.py
@@ -4,34 +4,12 @@
 # uncomment for debugging:
 # import matplotlib.pyplot as plt
 
-import numpy
+import numpy as np
 from scipy.fftpack import rfft
-from scipy.io.wavfile import read
-from scipy.signal import hann
+from scipy.io.wavfile import read as readwav
+from scipy.signal import spectrogram
 import sys
 import warnings
-
-def moving_average(a, w):
-    # calculate moving average
-    window = numpy.ones(int(w)) / float(w)
-    r = numpy.convolve(a, window, 'valid')
-    # len(a) = len(r) + w
-    a = numpy.empty((int(w / 2)))
-    a.fill(numpy.nan)
-    b = numpy.empty((int(w - len(a))))
-    b.fill(numpy.nan)
-    # add nan arrays to equal input and output length
-    return numpy.concatenate((a, r, b))
-
-
-def find_cutoff(a, dx, diff, limit):
-    for i in range(1, int(a.shape[0] - dx)):
-        if a[-i] / a[-1] > limit:
-            break
-    if a[int(-i - dx)] - a[-i] > diff:
-        return a.shape[0] - i - dx
-    return a.shape[0]
-
 
 # print usage if no argument given
 if len(sys.argv[1:]) < 1:
@@ -42,7 +20,7 @@ if len(sys.argv[1:]) < 1:
 try:
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
-    input_data = read(sys.argv[1])
+    input_data = readwav(sys.argv[1])
 except IOError as e:
     print(e[1])
     sys.exit(e[0])
@@ -51,54 +29,34 @@ except IOError as e:
 freq = input_data[0]
 audio = input_data[1]
 channel = 0
+toffset = 10
+tsample_size = 20
 samples = len(audio[:, 0])
 seconds = int(samples / freq)
-seconds = min(seconds, 30)
-spectrum = [0] * freq
+seconds = min(seconds, tsample_size)
+ssample_size = freq*seconds
 
-# run over the seconds (max 30)
-for t in range(0, seconds - 1):
-    # apply hanning window
-    window = hann(freq)
-    audio_second = audio[t * freq:(t + 1) * freq, channel] * window
-    # do fft to add second to frequency spectrum
-    spectrum += abs(rfft(audio_second))
+f,t,Sxx = spectrogram(audio[freq*toffset:ssample_size + (freq*toffset),
+    channel], freq)
+color_arr = 10*np.log(Sxx)
 
-# calculate average of the spectrum
-spectrum /= seconds
-# normalize frequency spectrum
-spectrum = numpy.lib.scimath.log10(spectrum)
-# smoothen frequency spectrum with window w
-spectrum = moving_average(spectrum, freq / 100)
-# find cutoff in frequency spectrum
-cutoff = find_cutoff(spectrum, freq / 50, 1.25, 1.1)
-# print bit rate of frequency spectrum before cutoff
-print('%i kHz' % (cutoff / 2000))
-print('(%s kHz)' % (cutoff / 2000.0))
-if (cutoff // 2000) == 11:
-    print('64 kbps')
-elif (cutoff // 2000) == 16:
-    print('128 kbps')
-elif (cutoff // 2000) == 19:
-    print('192 kbps')
-elif (cutoff // 2000) == 20:
-    print('320 kbps')
-elif (cutoff // 2000) == 22:
-    print('500 kbps')
-elif (cutoff * 100) / freq > 90:
-    print('lossless')
-else:
-    print("No Match")
+# Takes logified Sxx with range 0-100
+def get_cutoff(colors):
+    for i,row in enumerate(colors):
+        if np.quantile(row, 0.60) < -100:
+            return f[i]
+    return f[len(colors)-1]
 
+print(get_cutoff(color_arr))
 # debugging only:
+# Use this to label the frequencies appropriately
+# ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.2f}'.format(x/1000) + 'K'))
 if 'plt' in globals():
-    print("Cutoff found to be {}".format(cutoff))
     # plot
-    plt.plot(spectrum)
-    # label the axes
-    plt.ylabel('Magnitude')
-    plt.xlabel('Frequency')
+    plt.pcolormesh(t, f, color_arr, shading='gouraud')
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
     # set the title
-    plt.title('Spectrum')
-    plt.axis((0, 45000, 0, 10))
+    plt.title('Spectrogram')
     plt.show()
+
